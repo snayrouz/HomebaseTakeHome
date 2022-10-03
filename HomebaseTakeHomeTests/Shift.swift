465d4c5a678b78143fe79b58e2ebbaec88718bf4
Sam Nayrouz <samuel.nayrouz@gmail.com>
2022-10-03 12:49:28 -0600

    Add unit test for instantiating Shift

---
+++ HomebaseTakeHomeTests/Shift.swift
@@ -0,0 +1,31 @@
+//
+//  Shift.swift
+//  HomebaseTakeHome
+//
+//  Created by Nayrouz, Samuel on 10/3/22.
+//
+
+import XCTest
+
+final class Shift: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testShiftModelInstanceCreated() {
+        let shiftInstance = Shift(
+            role: "Waiter",
+            name: "Anna",
+            startDate: "2018-04-20 9:00:00 -08:00",
+            endDate: "2018-4-20 12:00:00 -08:00",
+            color: "red")
+        
+        XCTAssertNotNil(shiftInstance)
+    }
+
+}
