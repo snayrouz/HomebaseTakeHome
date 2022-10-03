528e249a4aa4beec2712cfe0aadf10817a565ca1
Sam Nayrouz <samuel.nayrouz@gmail.com>
2022-10-03 12:43:54 -0600

    Initial Commit

---
+++ HomebaseTakeHome/HomebaseTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomebaseTakeHomeApp.swift
+//  HomebaseTakeHome
+//
+//  Created by Nayrouz, Samuel on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct HomebaseTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
